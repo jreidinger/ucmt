1500ac17b5b8fcd153f4d58e1335968887c60f31
Josef Reidinger <jreidinger@suse.cz>
2021-03-22 09:50:34 +0100

    user cheetah and add support for passwords if there are permissions

--- lib/ucmt/discovery/local_users.rb
+++ lib/ucmt/discovery/local_users.rb
@@ -19,13 +19,24 @@ module UCMT
     private
 
       def read_users
-        res = JSON.parse(`ansible localhost -m getent -a database=passwd`.sub(/^.*=>/, ""))
+        output = Cheetah.run("ansible", "localhost", "-m", "getent", "-a", "database=passwd", stdout: :capture)
+
+        res = JSON.parse(output.sub(/^.*=>/, ""))
         res["ansible_facts"]["getent_passwd"]
       end
 
       def read_groups
-        res = JSON.parse(`ansible localhost -m getent -a database=group`.sub(/^.*=>/, ""))
+        output = Cheetah.run("ansible", "localhost", "-m", "getent", "-a", "database=group", stdout: :capture)
+
+        res = JSON.parse(output.sub(/^.*=>/, ""))
         res["ansible_facts"]["getent_group"]
+      end
+
+      def read_passwords
+        output = Cheetah.run("ansible", "localhost", "-m", "getent", "-a", "database=shadow", stdout: :capture)
+
+        res = JSON.parse(output.sub(/^.*=>/, ""))
+        res["ansible_facts"]["getent_shadow"]
       end
 
       USERS_KEYS_MAPPING = {
@@ -44,23 +55,33 @@ module UCMT
       }
 
       def read
+        # reading shadow need root permissions
+        # TODO: for remote check needs to be different
+        if Process.euid == 0
+          passwd = read_passwords
+        else
+          passwd = {}
+        end
+
+        groups = read_groups
+
         users = read_users.map do |dk, dv|
           USERS_KEYS_MAPPING.each_with_object({"name" => dk}) { |(k, v), r| r[k] = dv[v] }
         end
         users.each { |u| INTEGER_KEYS.each { |i| u[i] = u[i].to_i } }
-        users.select! { |v| v["uid"] > SYSTEM_USER_LIMIT } # select only non system users
-        users.each { |u| u["groups"] = [] }
+        users.select! { |v| v["uid"] == 0 || v["uid"] > SYSTEM_USER_LIMIT } # select only non system users
+        users.each do |user|
+          user["groups"] = []
 
-        groups = read_groups
-        groups.each do |name, group|
-          gid = group[GROUPS_KEYS_MAPPING["gid"]].to_i
-          group_users = group[GROUPS_KEYS_MAPPING["users"]].split(",") # see man group
+          groups.each do |name, group|
+            gid = group[GROUPS_KEYS_MAPPING["gid"]].to_i
+            group_users = group[GROUPS_KEYS_MAPPING["users"]].split(",") # see man group
 
-          users.each do |user|
             if user["gid"] == gid || group_users.include?(user["name"])
               user["groups"] << name
             end
           end
+          user["password"] = passwd[user["name"]].first if passwd[user["name"]]
         end
 
         users
